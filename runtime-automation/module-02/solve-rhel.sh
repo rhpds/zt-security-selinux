4ee4ef62ca08c194d5014de5b386b2aabcce06a9
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2026-04-27 10:10:14 -0700

    Set up HTTP server and handle SELinux configuration
    Install and configure HTTP server, analyze SELinux issues, and log progress.

--- runtime-automation/module-02/solve-rhel.sh
+++ runtime-automation/module-02/solve-rhel.sh
@@ -1,2 +1,33 @@
 #!/bin/sh
+
+sestatus
+
+cat /etc/selinux/config
+
+sudo dnf install httpd -y
+
+sudo systemctl enable --now httpd
+
+curl localhost
+
+echo "Test HTML file" > ~/index.html
+
+sudo mv ~/index.html /var/www/html/
+
+curl localhost
+
+ls -lZ /var/www/html/index.html
+
+sudo ausearch -m AVC -ts recent
+
+sudo dnf install setroubleshoot-server -y
+
+sudo sealert --analyze /var/log/audit/audit.log
+
+sudo semanage fcontext --list | grep '/var/www(/.*)?'
+
+sudo restorecon -Rv /var/www/html
+
+curl localhost
+
 echo "Solved module called module-02" >> /tmp/progress.log
